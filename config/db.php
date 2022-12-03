ebd3de25d2c17511dcfc138326a9cb4bc48722b6
KlPetro <107338572+KlPetro@users.noreply.github.com>
2022-12-03 17:23:05 +0200

    1

--- config/db.php
+++ config/db.php
@@ -10,5 +10,3 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
-<h1></h1>
--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,6 +1,6 @@
  <a href="#" class="js-colorlib-nav-toggle colorlib-nav-toggle"><i></i></a>
  <aside id="colorlib-aside" role="complementary" class="js-fullheight text-center">
-     <h1 id="colorlib-logo"><a href="index.html">elen<span>.</span></a></h1>
+     <h1 id="colorlib-logo"><a href="/?p=home">elen<span>.</span></a></h1>
      <nav id="colorlib-main-menu" role="navigation">
          <ul>
              <li <?php if (!isset($_GET['p']) || $_GET['p'] == 'home') : ?> class="colorlib-active" <?php endif; ?>><a href="/?p=home">Home</a></li>
@@ -15,15 +15,9 @@
      <div class="colorlib-footer">
          <p>
              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-             Copyright &copy;
-
-             <script>
+             Copyright &copy;<script>
                  document.write(new Date().getFullYear());
-             </script> All rights reserved | This template is
-             made with
-             <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-
-
+             </script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
          <ul>
              <li><a href="#"><i class="icon-facebook"></i></a></li>
              <li><a href="#"><i class="icon-twitter"></i></a></li>
